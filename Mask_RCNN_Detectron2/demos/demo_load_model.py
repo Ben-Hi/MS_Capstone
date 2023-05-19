ba1abdb88bbcf1a514c596d4242204803a527de0
Benjamin Hillen <benjaminrhillen@gmail.com>
2023-05-19 14:00:37 -0700

    changed demo_load_model to load the overfit model

--- Mask_RCNN_Detectron2/demos/demo_load_model.py
+++ Mask_RCNN_Detectron2/demos/demo_load_model.py
@@ -11,8 +11,9 @@ import utils
 if __name__ == "__main__":
     print("This module demonstrates how to load a model and use it for inference and evaluation.")
     
-    weights_path = "/home/hillenb/Desktop/MS_Capstone/Mask_RCNN_Detectron2/output/overfit_model_final.pth"
-    config_path = "/home/hillenb/Desktop/MS_Capstone/Mask_RCNN_Detectron2/configs/branch_mask_rcnn_R_50_FPN_3x_overfit.yaml"
+    weights_path = os.path.join(settings.MODELS_DIR, "overfit/overfit_model_final.pth")
+    overfit_model_dir = os.path.join(settings.MODELS_DIR, "overfit")
+    config_path = os.path.join(settings.CONFIG_DIR, "branch_mask_rcnn_R_50_FPN_3x_overfit.yaml")
     
     # register the branch dataset
     branch_metadata = utils.register_dataset(settings.WHOLE_DATASET_NAME, get_branch_dicts)
@@ -24,6 +25,6 @@ if __name__ == "__main__":
     cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.7   # set a custom testing threshold
     predictor = DefaultPredictor(cfg)
     
-    evaluator = COCOEvaluator(settings.WHOLE_DATASET_NAME, output_dir=settings.OUTPUT_DIR)
+    evaluator = COCOEvaluator(settings.WHOLE_DATASET_NAME, output_dir=overfit_model_dir)
     val_loader = build_detection_test_loader(cfg, settings.WHOLE_DATASET_NAME)
     print(inference_on_dataset(predictor.model, val_loader, evaluator))
